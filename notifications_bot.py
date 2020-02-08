2c6d939f6c938a8bafffa50e8060803d6f4cd38d
Igor Zakharov <igorzakhar@gmail.com>
2020-02-08 16:48:21 +0300

    change exception handling

--- notifications_bot.py
+++ notifications_bot.py
@@ -17,10 +17,20 @@ def receive_notification(url, token, timestamp=''):
     headers = {'Authorization': f'Token {token}'}
     payload = {'timestamp': timestamp}
 
-    response = requests.get(url, headers=headers, params=payload)
-    response.raise_for_status()
+    while True:
+        try:
+            response = requests.get(url, headers=headers, params=payload)
+            response.raise_for_status()
 
-    return response.json()
+            return response.json()
+
+        except (
+            requests.exceptions.ReadTimeout,
+            requests.exceptions.ConnectionError
+        ):
+
+            time.sleep(30)
+            continue
 
 
 def run_bot(api_url, api_token, chat_id, bot_token, proxy):
@@ -35,38 +45,31 @@ def run_bot(api_url, api_token, chat_id, bot_token, proxy):
     logger.info('Бот запущен.')
 
     while True:
-        try:
-            notice = receive_notification(api_url, api_token, timestamp)
 
-            timestamp = notice.get('timestamp_to_request', '')
+        notice = receive_notification(api_url, api_token, timestamp)
 
-            if notice.get('status') != 'found':
-                continue
+        timestamp = notice.get('timestamp_to_request', '')
 
-            for attempts in notice.get('new_attempts'):
-                lesson_title = attempts.get('lesson_title')
-                is_negative = attempts.get('is_negative')
-                result = (
-                    "Преподавателю все понравилось, "
-                    "можно приступать к следующему уроку."
-                )
-                if is_negative:
-                    result = "К сожалению, в работе нашлись ошибки."
-
-                notification_msg = (
-                    f'У вас проверили работу "{lesson_title}".\n\n{result}'
-                )
-
-                bot.send_message(chat_id=chat_id, text=notification_msg)
-
-            timestamp = notice.get('last_attempt_timestamp', '')
-
-        except Exception as err:
-            logger.error('Бот упал с ошибкой.')
-            logger.exception(err)
-
-            time.sleep(60)
+        if notice.get('status') != 'found':
             continue
+
+        for attempts in notice.get('new_attempts'):
+            lesson_title = attempts.get('lesson_title')
+            is_negative = attempts.get('is_negative')
+            result = (
+                "Преподавателю все понравилось, "
+                "можно приступать к следующему уроку."
+            )
+            if is_negative:
+                result = "К сожалению, в работе нашлись ошибки."
+
+            notification_msg = (
+                f'У вас проверили работу "{lesson_title}".\n\n{result}'
+            )
+
+            bot.send_message(chat_id=chat_id, text=notification_msg)
+
+        timestamp = notice.get('last_attempt_timestamp', '')
 
 
 def main():
@@ -84,7 +87,15 @@ def main():
 
     configure_logging(__file__, bot_token=tg_token, chat_id=chat_id)
 
-    run_bot(api_url, api_token, chat_id, tg_token, proxy_url)
+    while True:
+        try:
+            run_bot(api_url, api_token, chat_id, tg_token, proxy_url)
+
+        except Exception as err:
+            logger.error('Бот упал с ошибкой.')
+            logger.exception(err)
+
+            continue
 
 
 if __name__ == '__main__':
